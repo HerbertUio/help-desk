75bc8e9b00acaee49c69004b20b77f6d521f302e
Herbert Daniel <herbert.bustamante@uab.edu.bo>
2025-04-01 15:53:13 -0400

    Implementado HelpDeskDbContext, HelpDeskDbContextFactory Infrastructure

--- Infrastructure/Database/EntityFramework/Context/HelpDeskDbContext.cs
+++ Infrastructure/Database/EntityFramework/Context/HelpDeskDbContext.cs
@@ -1,6 +1,72 @@
+using Infrastructure.Database.EntityFramework.Entities;
+using Infrastructure.Database.EntityFramework.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+
 namespace Infrastructure.Database.EntityFramework.Context;
 
-public class HelpDeskDbContext
+public class HelpDeskDbContext: DbContext
 {
+    public DbSet<UserEntity> Users { get; set; }
     
+    public HelpDeskDbContext(DbContextOptions<HelpDeskDbContext> options): base(options)
+    {
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        // Configure the UserEntity
+        modelBuilder.Entity<UserEntity>(entity =>
+        {
+            entity.ToTable("Users", "CTR");
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Name).HasColumnName("name").HasColumnType("varchar(50)").IsRequired();
+            entity.Property(e => e.LastName).HasColumnName("last_name").HasColumnType("varchar(50)").IsRequired();
+            entity.Property(e => e.PhoneNumber).HasColumnName("phone_number").HasColumnType("varchar(20)").IsRequired();
+            entity.Property(e => e.Email).HasColumnName("email").HasColumnType("varchar(100)").IsRequired();
+            entity.Property(e => e.Password).HasColumnName("password").HasColumnType("varchar(500)").IsRequired();
+            entity.Property(e => e.DepartmentId).HasColumnName("department_id").IsRequired();
+            entity.Property(e => e.Role).HasColumnName("role").HasColumnType("varchar(50)").IsRequired();
+            entity.Property(e => e.Active).HasColumnName("active").HasColumnType("boolean").IsRequired();
+        });
+    }
+    public override int SaveChanges()
+    {
+        UpdateAuditFields();
+        return base.SaveChanges();
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        UpdateAuditFields();
+        return base.SaveChangesAsync(cancellationToken);
+    }
+    
+    private void UpdateAuditFields()
+    {
+        foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+        {
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    entry.Entity.CreatedAt = DateTime.UtcNow;
+                    entry.Entity.CreatedBy = GetCurrentUserId();
+                    entry.Entity.LastModifiedByAt = DateTime.UtcNow;
+                    entry.Entity.LastModifiedBy= GetCurrentUserId();
+                    break;
+
+                case EntityState.Modified:
+                    entry.Property(nameof(BaseEntity.CreatedAt)).IsModified = false;
+                    entry.Property(nameof(BaseEntity.CreatedBy)).IsModified = false;
+                    entry.Entity.LastModifiedByAt = DateTime.UtcNow;
+                    entry.Entity.LastModifiedBy = 104;
+                    break;
+            }
+        }
+    }
+    
+    private int GetCurrentUserId()
+    {
+        return 123;
+    }
 }
--- Infrastructure/Database/EntityFramework/Context/HelpDeskDbContextFactory.cs
+++ Infrastructure/Database/EntityFramework/Context/HelpDeskDbContextFactory.cs
@@ -1,6 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
 namespace Infrastructure.Database.EntityFramework.Context;
 
-public class HelpDeskDbContextFactory
+public class HelpDeskDbContextFactory : IDesignTimeDbContextFactory<HelpDeskDbContext>
 {
-    
+    public HelpDeskDbContext CreateDbContext(string[] args)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory()) 
+            .AddUserSecrets<HelpDeskDbContext>()  
+            .Build();
+        
+        string connectionString = configuration["ConnectionStrings:remoteConnection"];
+
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new ArgumentNullException(nameof(connectionString), "La cadena de conexión no puede ser nula ni estar vacía.");
+        }
+        
+        var optionsBuilder = new DbContextOptionsBuilder<HelpDeskDbContext>();
+        optionsBuilder.UseNpgsql(connectionString);
+        return new HelpDeskDbContext(optionsBuilder.Options);
+    }
 }
